7b6281d8b5f57487877fd7beba766853be5d94e0
hiroki <ouji.info@gmail.com>
2019-01-13 23:29:21 +0900

    .gitconfig追加

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -4,4 +4,5 @@ set -eu
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
-ln -fnsv ~/dotfiles/.config/peco/config.json ~/.config/peco/config.json
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.config/peco/config.json ~/.config/peco/config.json
