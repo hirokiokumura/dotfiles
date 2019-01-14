384987b8a0038d6106cf5ce6373e3d3a6a5771ef
hiroki <ouji.info@gmail.com>
2019-01-14 23:18:12 +0900

    dotfiles_link.shの実装を変更

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -5,13 +5,17 @@ if [ ! -L ~/.zprezto -a -d ~/dotfiles/prezto ]; then
     ln -s ~/dotfiles/prezto ~/.zprezto
 fi
 
-#ln -sf ~/dotfiles/.vimrc ~/.vimrc
-#ln -sf ~/dotfiles/.zshrc ~/.zshrc
-#ln -sf ~/dotfiles/Brewfile ~/Brewfile
-#ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.config/peco/config.json ~/.config/peco/config.json
 
-for file in .z* .vimrc Brewfile .gitconfig ; do
-    rm -i ~/$file
-    ln -s $PWD/$file ~/$file
+FILELIST="
+.z*
+.vimrc
+Brewfile
+.gitconfig
+"
+
+for FILE in ${FILELIST};
+do
+    rm -rf ~/${FILE}
+    ln -s $PWD/${FILE} ~/${FILE}
 done
